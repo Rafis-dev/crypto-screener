50bf3b9c89ba14c2efde66d4af0f60812fc3349c
Rafis Shigapov <rafis1work@gmail.com>
2026-08-15 10:14:59 +0300

    added initial home page details

---
+++ types.d.ts
@@ -0,0 +1,35 @@
+interface DataTableProps<T> {
+  columns: DataTableColumn<T>[];
+  data: T[];
+  rowKey: (row: T, index: number) => React.Key;
+  tableClassName?: string;
+  headerClassName?: string;
+  headerRowClassName?: string;
+  headerCellClassName?: string;
+  bodyRowClassName?: string;
+  bodyCellClassName?: string;
+}
+
+interface DataTableColumn<T> {
+  header: React.ReactNode;
+  cell: (row: T, index: number) => React.ReactNode;
+  headClassName?: string;
+  cellClassName?: string;
+}
+
+interface TrendingCoin {
+  item: {
+    id: string;
+    name: string;
+    symbol: string;
+    market_cap_rank: number;
+    thumb: string;
+    large: string;
+    data: {
+      price: number;
+      price_change_percentage_24h: {
+        usd: number;
+      };
+    };
+  };
+}
